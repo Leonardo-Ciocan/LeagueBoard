4eca1f5fbba78e035fc032ffa866109ac35153e4
Leonardo-Ciocan <leonardo.ciocan@outlook.com>
2015-02-19 18:47:10 +0000

    small changes to prepare for item segmentation

--- components.js
+++ components.js
@@ -5,13 +5,19 @@ var Item = React.createClass({
       },
       componentDidMount: function() {
           $.getJSON( "https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item/"+ this.props.id+"?api_key=6e0d8785-c4b5-41f1-b694-8567d1afdb8c", function( dt ) {
-          console.log(dt);
-          this.setState({data:dt});
+             // dt.description = dt.description.replace(/<[^>]*>/g ," \n");
+
+              console.log(dt.description);
+                this.setState({data:dt});
 }.bind(this));
           
           
       }, 
       render: function() {
+          //this.state.data.description.replace("<stat>" ,"</br");
+          // txt = this.state.data.description;
+          //console.log(txt);
+          //txt = txt.replace("<stat>" ,"</br");
           var url ="/item/"+this.state.data.id +".png";
             return <div className={"item " + (this.state.top?"topZ":"bottomZ")}>
                 <img src={url} onMouseOver={this.hover} onMouseOut={this.out}/>
@@ -43,5 +49,5 @@ var Items = React.createClass({
 });
 
 $(document).ready(function(){
-    React.render(<Items ids={[3136,3135,3134]} />, document.body);
+    React.render(<Items ids={[3136,3135,3134,1043,1051,1056]} />, document.body);
 });
